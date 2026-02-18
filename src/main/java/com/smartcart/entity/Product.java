3546e1721ba90cdfa49ed15f6eb5cf49d952926b
kalyanramnadhagiri <kr2235520@gmail.com>
2026-02-18 10:44:38 +0530

    Created Entity Classes

--- src/main/java/com/smartcart/entity/Product.java
+++ src/main/java/com/smartcart/entity/Product.java
@@ -1,7 +1,29 @@
-
 package com.smartcart.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@RequiredArgsConstructor
+@Table(name="product")
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 30)
+	private String name;
+	private String description;
+	private int stock;
+	private double price;
+	private String category;
+
 
 }
 
