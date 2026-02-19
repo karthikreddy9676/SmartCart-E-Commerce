2dfc5df751191e500ac4c3a68c708272a971df3a
karth <karth@BittuReddy>
2026-02-19 09:50:32 +0530

    Merge branch 'master' of https://github.com/karthikreddy9676/SmartCart-E-Commerce.git

--- src/main/java/com/smartcart/entity/User.java
+++ src/main/java/com/smartcart/entity/User.java
@@ -1,5 +1,56 @@
 package com.smartcart.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    // USER or ADMIN
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // account active status
+    private Boolean enabled = true;
+
+    // address info
+    private String address;
+    private String city;
+    private String state;
+   
+
+    // orders placed by user
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    // account creation time
+    private LocalDateTime createdAt;
+
+    // last updated time
+    private LocalDateTime updatedAt;
+
+   
 }
