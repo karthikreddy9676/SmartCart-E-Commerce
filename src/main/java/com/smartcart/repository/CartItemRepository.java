413ae0b28d2d45a0887ca527b7fed7a08f5f9c2a
karthik <karthikreddyb144@gmail.com>
2026-02-22 23:55:26 +0530

    total Repositories created

---
+++ src/main/java/com/smartcart/repository/CartItemRepository.java
@@ -0,0 +1,12 @@
+package com.smartcart.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smartcart.entity.CartItem;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+	Optional<CartItem> findByCart_CartIdAndProduct_ProductId(Long cartId, Long productId);
+}
---
+++ src/main/java/com/smartcart/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.smartcart.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smartcart.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	  Optional<Product> findByProductName(String productName);
+
+	    Page<Product> findByCategory_CategoryId(Long categoryId, Pageable pageable);
+
+	    Page<Product> findByCategory_CategoryName(String categoryName, Pageable pageable);
+
+	    Page<Product> findByProductNameContainingIgnoreCase(String keyword, Pageable pageable);
+
+}
