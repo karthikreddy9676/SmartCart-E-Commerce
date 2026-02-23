e52195ea240d7b4a8331c2f958d42c110ce53f9f
karthik <karthikreddyb144@gmail.com>
2026-02-23 09:50:39 +0530

    DTO updated

--- src/main/java/com/smartcart/serviceimpl/CartServiceImpl.java
+++ src/main/java/com/smartcart/serviceimpl/CartServiceImpl.java
@@ -1,7 +1,46 @@
 package com.smartcart.serviceimpl;
 
+import java.util.List;
+
+import com.smartcart.dto.CartDTO;
 import com.smartcart.service.CartService;
 
 public class CartServiceImpl implements CartService {
+
+	@Override
+	public CartDTO addProductToCart(Long productId, Integer quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CartDTO> getAllCarts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CartDTO getCart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CartDTO updateProductQuantityInCart(Long productId, Integer quantity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteProductFromCart(Long cartId, Long productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateProductInCarts(Long cartId, Long productId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
