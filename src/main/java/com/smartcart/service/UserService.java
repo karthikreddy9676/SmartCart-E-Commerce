d50e7eaf56df5bbc4c3bf7c1d0968b3a87d7399e
karthik <karthikreddyb144@gmail.com>
2026-02-26 09:35:58 +0530

    karthik

--- src/main/java/com/smartcart/service/UserService.java
+++ src/main/java/com/smartcart/service/UserService.java
@@ -1,7 +1,11 @@
-
 package com.smartcart.service;
 
+
+import com.smartcart.dto.UserDTO;
+
+
 public interface UserService {
+    UserDTO fetchCurrentlyLoggedInUserDetails();
 
+    UserDTO updateUserDetails(UserDTO userDTO, Long userId);
 }
-
--- src/main/java/com/smartcart/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/smartcart/serviceimpl/UserServiceImpl.java
@@ -1,7 +1,50 @@
 package com.smartcart.serviceimpl;
 
-import com.smartcart.service.UserService;
+import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.smartcart.dto.UserDTO;
+import com.smartcart.entity.User;
+import com.smartcart.exception.ResourceNotFoundException;
+import com.smartcart.repository.UserRepository;
+import com.smartcart.service.UserService;
+import com.smartcart.util.AuthUtil;
+
+@Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public UserDTO fetchCurrentlyLoggedInUserDetails() {
+        User loggedInUser = authUtil.loggedInUser();
+        Optional<User> optionalUser = userRepository.findById(loggedInUser.getUserId());
+        return modelMapper.map(optionalUser.get(), UserDTO.class);
+    }
+
+    @Override
+    public UserDTO updateUserDetails(UserDTO userDTO, Long userId) {
+        User existingUser = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(String.format("User not found with ID: ", userId)));
+        if (userDTO.getUserName() != null)
+            existingUser.setUserName(userDTO.getUserName());
+
+        if (userDTO.getEmail() != null)
+            existingUser.setEmail(userDTO.getEmail());
+
+        User updatedUser = userRepository.save(existingUser);
+        return modelMapper.map(updatedUser, UserDTO.class);
+    }
+
+	
 }
